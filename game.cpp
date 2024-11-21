66297d131b4098efba5ebd536dfb461e9b8a6a1a
Grace Kim <gracekim@Graces-MacBook-Air.local>
2024-11-20 16:45:06 -0900

    colors

--- game.cpp
+++ game.cpp
@@ -4,10 +4,23 @@
 #include <cstdlib>    // For rand() and srand()
 #include <ctime>      // For time()
 
-using namespace std;
+using std::time;
+using std::rand;
+using std::srand;
+
 
 const int WIDTH = 80;   // Width of the field
 const int HEIGHT = 25;  // Height of the field
+
+const int PLAYER_PAIR = 1;
+const int SPECIAL_PAIR = 2;
+const int WALL_COUNT = 750;
+
+const char WALL_CHAR = 'H';
+const char PLAYER_CHAR = '@';
+const char SPECIAL_CHAR = '#';
+const char EMPTY_CHAR = ' ';
+const char BORDER_CHAR = '*';
 
 
 void drawField(char field[HEIGHT][WIDTH], int playerX, int playerY) {
@@ -17,10 +30,18 @@ void drawField(char field[HEIGHT][WIDTH], int playerX, int playerY) {
         for (int x = 0; x < WIDTH; x++) {
             if (x == playerX && y == playerY)
             {
-                mvaddch(y, x, '@');  // Draw the player character
+                attron(COLOR_PAIR(PLAYER_PAIR));    // Player color
+                mvaddch(y, x, PLAYER_CHAR);         // Draw the player character
+                attroff(COLOR_PAIR(PLAYER_PAIR));   // Turn color off
+            }
+            else if(field[y][x] == SPECIAL_CHAR)
+            {
+                attron(COLOR_PAIR(SPECIAL_PAIR));   // Special color
+                mvaddch(y, x, SPECIAL_CHAR);        // Draw the special character
+                attroff(COLOR_PAIR(SPECIAL_PAIR));
             }
             else
-                mvaddch(y, x, field[y][x]);  // Draw the field
+                mvaddch(y, x, field[y][x]);         // Draw the field
         }
     }
     refresh();  // Refresh the screen to reflect changes
@@ -31,14 +52,14 @@ void drawBorder() {
 
     // Draw top and bottom borders
     for (int x = 0; x < WIDTH; x++) {
-        mvaddch(0, x, '*');              // Top border
-        mvaddch(HEIGHT - 1, x, '*');     // Bottom border
+        mvaddch(0, x, BORDER_CHAR);              // Top border
+        mvaddch(HEIGHT - 1, x, BORDER_CHAR);     // Bottom border
     }
 
     // Draw left and right borders
     for (int y = 0; y < HEIGHT; y++) {
-        mvaddch(y, 0, '*');              // Left border
-        mvaddch(y, WIDTH - 1, '*');      // Right border
+        mvaddch(y, 0, BORDER_CHAR);              // Left border
+        mvaddch(y, WIDTH - 1, BORDER_CHAR);      // Right border
     }
 
     refresh();  // Refresh the screen
@@ -52,7 +73,7 @@ void placeSpecialTile(char field[HEIGHT][WIDTH], int& specialX, int& specialY, i
         specialY = rand() % HEIGHT;  // Random row
     } while (specialX == playerX && specialY == playerY);  // Avoid placing on the player's position
 
-    field[specialY][specialX] = '#';  // Place the special tile
+    field[specialY][specialX] = SPECIAL_CHAR;  // Place the special tile
 }
 
 void placeWallTile(char field[HEIGHT][WIDTH], int& wallX, int& wallY, int playerX, int playerY) {
@@ -60,7 +81,7 @@ void placeWallTile(char field[HEIGHT][WIDTH], int& wallX, int& wallY, int player
         wallX = rand() % WIDTH;  // Random column
         wallY = rand() % HEIGHT;  // Random row
     } while (wallX == playerX && wallY == playerY && wallX < WIDTH/2 && wallY < HEIGHT/2);  // Avoid placing on the player's position
-    field[wallY][wallX] = 'I';  // Place the special tile
+    field[wallY][wallX] = WALL_CHAR;  // Place the special tile
 }
 
 int main() {
@@ -71,12 +92,22 @@ int main() {
     keypad(stdscr, true);  // Enable arrow keys
     noecho();              // Disable key echoing
 
+    if(has_colors() == FALSE) // Check for colors
+	{	
+        endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+	start_color();         //Start color 		
+    init_pair(PLAYER_PAIR, COLOR_CYAN, COLOR_BLACK);
+    init_pair(SPECIAL_PAIR, COLOR_RED, COLOR_BLACK);
+
     char field[HEIGHT][WIDTH];
 
     // Initialize the field with dots
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            field[y][x] = ' ';  // Fill the field with dots
+            field[y][x] = EMPTY_CHAR;  // Fill the field with dots
         }
     }
 
@@ -85,7 +116,7 @@ int main() {
 
     int wallX, wallY;
     //places walls randomly
-    for(int i = 1; i< 900; ++i)
+    for(int i = 1; i< WALL_COUNT; ++i)
     placeWallTile(field, wallX, wallY, playerX, playerY);
 
     // Place the special tile randomly
@@ -99,16 +130,16 @@ int main() {
 
         switch (key) {
             case KEY_UP:  // Arrow Up
-                if (playerY > 0 && field[playerY-1][playerX] != 'I') playerY--;
+                if (playerY > 0 && field[playerY-1][playerX] != WALL_CHAR) playerY--;
                 break;
             case KEY_DOWN:  // Arrow Down
-                if (playerY < HEIGHT - 1 && field[playerY+1][playerX] != 'I') playerY++;
+                if (playerY < HEIGHT - 1 && field[playerY+1][playerX] != WALL_CHAR) playerY++;
                 break;
             case KEY_LEFT:  // Arrow Left
-                if (playerX > 0 && field[playerY][playerX-1] != 'I') playerX--;
+                if (playerX > 0 && field[playerY][playerX-1] != WALL_CHAR) playerX--;
                 break;
             case KEY_RIGHT:  // Arrow Right
-                if (playerX < WIDTH - 1 && field[playerY][playerX+1] != 'I') playerX++;
+                if (playerX < WIDTH - 1 && field[playerY][playerX+1] != WALL_CHAR) playerX++;
                 break;
             case 27:  // Escape key to exit
                 endwin();  // End ncurses mode
@@ -125,7 +156,7 @@ int main() {
             playerY = HEIGHT / 2;
             for (int y = 0; y < HEIGHT; y++) {  // Clear the previous special tile
                 for (int x = 0; x < WIDTH; x++) {
-                    if (field[y][x] == '#') field[y][x] = ' ';
+                    if (field[y][x] == SPECIAL_CHAR) field[y][x] = EMPTY_CHAR;
                 }
             }
             placeSpecialTile(field, specialX, specialY, playerX, playerY);
